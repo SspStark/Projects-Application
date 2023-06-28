93f6b39efe9e5403ebfba9be8e69dbc64bb38c18
SspStark <ssptonystark005@gmail.com>
2023-06-28 11:27:43 +0000

    identifying state and updating styles based on state

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, clickTabItem, isActive} = props
+  const {tabId, displayText} = tabDetails
+  const onClickTabItem = () => {
+    clickTabItem(tabId)
+  }
+  const activeTabBtn = isActive ? 'active-tab-btn' : ''
+
+  return (
+    <li className="tab-item-container ">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabBtn}`}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
